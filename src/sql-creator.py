3f7b2af79e3c85709104804a918bbf838a3d1b7a
StoicaGabriel <gabriel.stoica@numeract.com>
2020-04-20 17:46:06 +0300

    [wip] added tables

--- src/sql-creator.py
+++ src/sql-creator.py
@@ -28,7 +28,48 @@ class Student(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(30), unique=False, nullable=False)
     surname = db.Column(db.String(30), unique=False, nullable=False)
-    # address = db.
+    address = db.Column(db.String(200), unique=False, nullable=True)
+    year_of_study = db.Column(db.String(2), unique=False, nullable=True)
+    adm_score = db.Column(db.Float, unique=False, nullable=False)
+    # Current exam scores for all past years and semesters
+    scores = db.Column(db.String(200), unique=False, nullable=True)
+    # Foreign key to a StudyClass instance
+    study_class = db.Column()  # TODO: fill foreign key info
+
+    st_class = db.relationship()  # TODO: fill relationship info
+
+
+class Professor(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), unique=False, nullable=False)
+    surname = db.Column(db.String(30), unique=False, nullable=False)
+    # Rank of the teacher eg. PhD...
+    merits = db.Column(db.String(10), unique=False, nullable=False)
+    study_classes = db.Column()  # TODO: vector type which links to multiple StudyClass objects
+
+
+class StudyClass(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(10), unique=True, nullable=False)
+    year_of_study = db.Column(db.String(2), unique=False, nullable=False)
+    schedule = db.Column()  # TODO: fill foreign key info
+
+
+class Schedule(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    # This is where the schedule is written for every week's day
+    schedule = db.Column(db.String(200), unique=False, nullable=True)
+
+
+# Not implemented, TBD if needs to be added
+# class Discipline(db.Model):
+#     id = db.Column
+#     # Name is unique, tough multiple teachers can be assigned for different classes
+#     name = db.Column(db.String(20), unique=True, nullable=False)
+#     # Day of the week + hour interval of the class
+#     course_schedule = db.Column(db.String(20), unique=False, nullable=True)
+#     lab_schedule = db.Column(db.String(20), unique=False, nullable=True)
+#     seminary_schedule = db.Column(db.String(20), unique=False, nullable=True)
 
 
 if __name__ == '__main__':
