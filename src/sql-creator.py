082e293caf5c599b75a3fe98f7eaaf62c105888f
StoicaGabriel <gabriel.stoica@numeract.com>
2020-04-21 21:49:07 +0300

    updated db logic

--- src/sql-creator.py
+++ src/sql-creator.py
@@ -34,10 +34,10 @@ class Student(db.Model):
     # Current exam scores for all past years and semesters
     scores = db.Column(db.String(200), unique=False, nullable=True)
     # Foreign key to a StudyClass instance
-    study_class = db.Column(db.Integer, db.ForeignKey('st_class.id'))
+    study_class_id = db.Column(db.Integer, db.ForeignKey('study_class.id'))
 
     # Foreign key relationship
-    st_class = db.relationship('StudyClass', backref=db.backref('students', lazy=True))
+    study_class = db.relationship('StudyClass', backref=db.backref('students', lazy=True))
 
 
 class Professor(db.Model):
@@ -46,16 +46,21 @@ class Professor(db.Model):
     surname = db.Column(db.String(30), unique=False, nullable=False)
     # Rank of the teacher eg. PhD...
     merits = db.Column(db.String(10), unique=False, nullable=False)
-    study_classes = db.Column()  # TODO: vector type which links to multiple StudyClass objects
+
+    # Relationship for foreign key in a different table
+    study_classes = db.relationship('StudyClass', backref='professor')
 
 
 class StudyClass(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(10), unique=True, nullable=False)
     year_of_study = db.Column(db.String(2), unique=False, nullable=False)
-    schedule = db.Column(db.Integer, db.ForeignKey('sc'))
 
-    sc = db.relationship('Schedule', backref=db.backref('classes', lazy=True))
+    schedule_id = db.Column(db.Integer, db.ForeignKey('schedule.id'))
+    professor_id = db.Column(db.Integer, db.ForeignKey('professor.id'))
+
+    schedule = db.relationship('Schedule', backref=db.backref('classes', lazy=True))
+    discipline = db.relationship('Discipline', backref='study_class')
 
 
 class Schedule(db.Model):
@@ -63,17 +68,22 @@ class Schedule(db.Model):
     # This is where the schedule is written for every week's day
     schedule = db.Column(db.String(200), unique=False, nullable=True)
 
+    disciplines = db.relationship('Discipline', backref='schedule')
+
 
 # Not implemented, TBD if needs to be added
-# class Discipline(db.Model):
-#     id = db.Column
-#     # Name is unique, tough multiple teachers can be assigned for different classes
-#     name = db.Column(db.String(20), unique=True, nullable=False)
-#     # Day of the week + hour interval of the class
-#     course_schedule = db.Column(db.String(20), unique=False, nullable=True)
-#     lab_schedule = db.Column(db.String(20), unique=False, nullable=True)
-#     seminary_schedule = db.Column(db.String(20), unique=False, nullable=True)
+class Discipline(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), unique=False, nullable=False)
+    # Day of the week + hour interval of the class
+    course_schedule = db.Column(db.String(20), unique=False, nullable=True)
+    lab_schedule = db.Column(db.String(20), unique=False, nullable=True)
+    seminary_schedule = db.Column(db.String(20), unique=False, nullable=True)
+
+    schedule_id = db.Column(db.Integer, db.ForeignKey('schedule.id'))
+    study_class_id = db.Column(db.Integer, db.ForeignKey('study_class.id'))
 
 
 if __name__ == '__main__':
+    # TODO: cleanup useless fields & separate function for creating everything
     db.create_all()
